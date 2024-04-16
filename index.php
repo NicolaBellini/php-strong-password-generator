bfd69be5f10067788b49aa98260bbf5c766f50bc
NicolaBellini <bellininicola4366@gmail.com>
2024-04-16 14:09:02 +0200

    [add] array with all characters

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php 
+
+isset($_GET['passLength']);
+var_dump($_GET['passLength']);
+
+$carachters=[
+  'letters'=>[
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  ],
+  'maiusc'=>[
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  ],
+  'symbols'=>[
+    '!', '?', '&', '%', '$', '<', '>', '^', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@', '#', '_', '='
+  ],
+  'numbers'=>[
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+  ],
+]
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +34,22 @@
   <title>title</title>
 </head>
 <body>
-  ciaos
+
+<div class="container p-5">
+  <form action="index.php" method="get">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="passLength">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    
+   
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form> 
+</div>
+
+
+
+
 </body>
 </html>
